5b5cd869a9315713e0b875574138926fd9e2776b
Timo <43245438+TheRisenPhoenix@users.noreply.github.com>
2024-07-10 10:11:50 +0200

    Change yolo baseline

---
+++ detect.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO("models/combined/weights/best.pt")
+    # result = model("data/CLINIC_TEST_SR_100/images/D_test_sr/SR_p2_og_hf_vf_55.png")
+    # print(result)
+    # result[0].show()
+    metrics = model.val(data="data/guidewiredetection_combined.yaml", split="test", save_json=True)
+    print(metrics.box.map)  # mAP50-95
+    print(metrics.box.map50)  # mAP50
+    print(metrics.box.map75)  # mAP75
+    print(metrics.box.maps)  # list of mAP50-95 for each category
